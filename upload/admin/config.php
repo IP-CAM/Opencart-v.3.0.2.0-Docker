73f3b88c1a61fef5fae32a3a7ff91a152982b0fc
jingjia88 <renee.ch48@gmail.com>
2018-07-10 20:30:05 +0800

    chinese

--- upload/admin/config.php
+++ upload/admin/config.php
@@ -25,7 +25,7 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', '172.22.0.6');
+define('DB_HOSTNAME', 'mysql');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'opencart');
