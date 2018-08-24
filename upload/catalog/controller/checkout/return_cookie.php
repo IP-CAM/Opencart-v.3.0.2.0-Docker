7353fdaf53222f2400cfa4a778f055d259a6706c
jingjia88 <renee.ch48@gmail.com>
2018-08-24 20:31:45 +0800

    cookie

---
+++ upload/catalog/controller/checkout/return_cookie.php
@@ -0,0 +1,10 @@
+<?php
+class ControllerCheckoutReturnCookie extends Controller {
+	public function index() {
+        $cookie['route']  = $_COOKIE['route'];
+        $cookie['name']  = $_COOKIE['name'];
+        $cookie['address']  = $_COOKIE['address'];
+
+        return $cookie;
+	}	
+}
